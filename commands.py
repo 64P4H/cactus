68a07528517e2f198ed520553a5d23d8c5316cdc
64P4H <45884769+64P4H@users.noreply.github.com>
2018-12-29 13:45:45 +0300

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,27 @@
+command_to_recognize={
+	('вот', 'бот', 'both', 'what', 'nico', 'неку', 'nickel'):('.\shortcuts\\runbot.bat.lnk'),
+	('дискорд', 'дискорт', 'discord', 'escort')				:('.\shortcuts\Discord.lnk'),
+	('стим', 'steam', 'tsum', 'jim')						:('.\shortcuts\Steam.lnk'),
+	('aimp', "i'm", 'плеер', 'player', 'blair')				:('.\shortcuts\AIMP.lnk'),
+	('программирование', 'тонирование')						:('.\shortcuts\cmd.exe.lnk', '.\shortcuts\sublime_text.exe.lnk', '.\shortcuts\explorer.exe.lnk'),
+	('гитхаб', 'hop', 'cop')								:('.\shortcuts\GitHub Desktop.lnk'),
+	('skvirel', 'эскель', 'scared', 'sql', 'sq', 'quelle')	:('.\shortcuts\Database5Pro.exe.lnk')
+#	
+#	
+#	
+}
+
+
+
+keyboard_event_writes={
+	'ctrl+shift+x+0':'¯\_(ツ)_/¯',
+	'ctrl+shift+x+1':'(☉_☉)',
+	'ctrl+shift+x+2':'(⊙_☉)',
+	'ctrl+shift+x+3':'(╯°□°）╯︵ ┻━┻',
+	'ctrl+shift+x+4':"┻━┻ ︵ ＼\('0')/／ ︵ ┻━┻",
+	'ctrl+shift+x+5':'o(╥﹏╥)o',
+	'ctrl+shift+x+6':'(ง •̀_•́)ง',
+	'ctrl+shift+x+7':'(ᗒᗣᗕ)՞',
+	'ctrl+shift+x+8':'༼ つ ◕_◕ ༽つ',
+	'ctrl+shift+x+9':'( ＾◡＾)っ✂╰⋃╯'
+}
